9d8778a0f4e088b84bbb43cfda06d578d1313efa
basmarle <basvanmarle@hotmail.com>
2016-03-17 21:33:07 +0100

    Latest working version

--- AVRGCC1/AVRGCC1.c
+++ AVRGCC1/AVRGCC1.c
@@ -6,20 +6,27 @@
  */ 
 
 #include <stdlib.h>
-#include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define MAX_TASKS 2
-
+//Counter gebruikt voor algemene doeleinde
 volatile int counter = 0;
+//Verteld de scheduler dat het de eerste keer is
 volatile int firstrun = 1;
+//Geeft aan dat de scheduler is aangeroepen door een geblokkerde taak
 volatile int returnFromBlocked = 0;
+//Aantal taken in de ready lijst
+volatile int amountReadyTasks = 0;
+//Aantal geblokkerde taken
+volatile int amountBlockedTasks = 0;
+//Aantal taken
+volatile int taskCount = 0;
 
+//Settings for the kernel(ISR)
 typedef struct{
 	int isrTicks;
 } kernel_settings_t;
 
-//Task Program Block
+//Task Control Block
 typedef struct{
 	int id;
 	//Stack pointer high
@@ -39,8 +46,6 @@ typedef struct{
 	int markedforremoval;
 	//Sleep counter
 	long sleepCounter;
-	//temp var for sleeping tests
-	int sleeping;
 } task_table_t;
 
 //DoubleLinked list for all the tasks
@@ -51,11 +56,10 @@ struct node{
 };
 
 struct node *root;
-struct node *current_node;
-struct node *previous_node;
+struct node *current_node_ready;
 struct node *temp;
+struct node *blockedQueue[10];
 
-task_table_t task_table[MAX_TASKS];
 kernel_settings_t kernel_settings;
 
 register unsigned char reg1 asm("r2");
@@ -64,17 +68,21 @@ register unsigned char reg2 asm("r3");
 //Declare the ISR as a naked function
 ISR (TIMER1_COMPA_vect) __attribute__ ((naked));
 void * sleep(long time) __attribute__((naked));
-
+//void * initTask(void * taskAddress) __attribute__ ((naked));
 //Task1
 void * task1(){
 	long a = 0;
 	while(1){
 		a++;
+		//
 		PORTB = (0 << PINB5);
 		a = 0;
-		sleep(10);
+		//
+		sleep(20);
+		//
 		PORTB = (1 << PINB5);
-		sleep(10);
+		//
+		sleep(20);
 	}
 }
 
@@ -90,45 +98,47 @@ void * task2(){
 
 //Task3
 void * task3(){
-	current_node->task->markedforremoval = 1;
+	//
+	current_node_ready->task->markedforremoval = 1;
 	//End of task, jump to ISR
 	asm("ijmp" :: "z" (TIMER1_COMPA_vect));
 }
 
 void initTask(void * taskAddress){
-	static int taskCount = 0;
 	
 	//Only use the root for the first task
 	if(taskCount == 0){
-		root->task = malloc(sizeof(task_table_t));
-		root->next = 0;
+		current_node_ready->task = malloc(sizeof(task_table_t));
+		current_node_ready->next = current_node_ready;
+		current_node_ready->previous =  current_node_ready;
 	} else {
-		current_node->next = malloc(sizeof(task_table_t));
-		current_node->next->previous = current_node;
-		current_node = current_node->next;
-		current_node->task = malloc(sizeof(task_table_t));
-		current_node->next = 0;
+		current_node_ready->next = malloc(sizeof(task_table_t));
+		current_node_ready->next->previous = current_node_ready;
+		current_node_ready = current_node_ready->next;
+		current_node_ready->task = malloc(sizeof(task_table_t));
+		current_node_ready->next = 0;
 	}
 	
 	//Set ID of task
-	current_node->task->id = taskCount;
+	current_node_ready->task->id = taskCount;
 	//Start address of the task
-	current_node->task->address = taskAddress;
+	current_node_ready->task->address = taskAddress;
 	//Split into 2 bytes
-	current_node->task->pcl = ((int)current_node->task->address & 0x00FF);
-	current_node->task->pch = ((int)current_node->task->address & 0xFF00) >> 8;
+	current_node_ready->task->pcl = ((int)current_node_ready->task->address & 0x00FF);
+	current_node_ready->task->pch = ((int)current_node_ready->task->address & 0xFF00) >> 8;
 	//Set task pointer
-	current_node->task->spl = (0x7D0 - (taskCount * 0x64)) & 0x00FF;
-	current_node->task->sph = ((0x7D0 - (taskCount * 0x64)) & 0xFF00) >> 8;
+	current_node_ready->task->spl = (0x7D0 - (taskCount * 0x64)) & 0x00FF;
+	current_node_ready->task->sph = ((0x7D0 - (taskCount * 0x64)) & 0xFF00) >> 8;
 	//Always first time
-	current_node->task->firsttime = 1;
+	current_node_ready->task->firsttime = 1;
 	//Not suitable for removal
-	current_node->task->markedforremoval = 0;
+	current_node_ready->task->markedforremoval = 0;
 	//Set sleep counter 0
-	current_node->task->sleepCounter = 0;
-	//Temp var for sleeping test
-	current_node->task->sleeping = 0;
+	current_node_ready->task->sleepCounter = 0;
+	//Amount of overall tasks
 	taskCount++;
+	//Amount of tasks in the ready queue
+	amountReadyTasks++;
 }
 
 int main(void)
@@ -156,15 +166,15 @@ int main(void)
 	TIMSK1 |= (1 << OCIE1A);
 	
 	//Create a root TCB
-	root = malloc(sizeof(struct node));
-	current_node  = root;
+	current_node_ready = malloc(sizeof(struct node));
+	root  = current_node_ready;
 	initTask(task1);
-	temp = current_node;
+	temp = current_node_ready;
 	initTask(task2);
 	initTask(task3);
-	current_node->next = root;
-	root->previous = current_node;
-	current_node  = root;
+	current_node_ready->next = root;
+	root->previous = current_node_ready;
+	current_node_ready  = root;
 
 	//allow interrupts
 	sei();
@@ -179,12 +189,21 @@ int main(void)
 
 
 void * sleep(long time){
-	current_node->task->sleepCounter = (time * kernel_settings.isrTicks) * 2;
-	current_node->previous->next = current_node->next;
-	current_node->next->previous = current_node->previous;
-	current_node->task->sleeping = 1;
+	//Disable interrupts
+	cli();
+	//Set the sleep time in the TCB
+	current_node_ready->task->sleepCounter = (time * kernel_settings.isrTicks) * 2;
+	//Remove the task from the ready list
+	current_node_ready->previous->next = current_node_ready->next;
+	current_node_ready->next->previous = current_node_ready->previous;
+	//Add the task to the blocked queue
+	blockedQueue[amountBlockedTasks] = current_node_ready;
+	amountBlockedTasks++;
 	//Tell the ISR we are comming back early because a task is blocked
 	returnFromBlocked = 1;
+	//Enable interrupts
+	sei();
+	//Jump to the ISR
 	asm("ijmp" :: "z" (TIMER1_COMPA_vect));
 }
 
@@ -194,6 +213,8 @@ ISR (TIMER1_COMPA_vect)
 	//Disable interrupts
 	cli();
 	
+	//If the isr is called because a task has ended prematurely
+	//we are going to switch tasks so we enable the dispatcher
 	if(returnFromBlocked){
 		dispatcher = 1;
 		returnFromBlocked = 0;
@@ -201,40 +222,48 @@ ISR (TIMER1_COMPA_vect)
 		//Toggle dispatcher every 1 ms
 		dispatcher ^= 1;
 	}
-	//update timers
-	//TODO
-	if(root->task->sleepCounter > 0){
-		root->task->sleepCounter -= (kernel_settings.isrTicks);
-	} else if(root->task->sleeping){
-		//Reset the sleep if < 0
-		root->task->sleepCounter = 0;
-		current_node->next = root;
-		current_node->previous = root;
-		root->next = current_node;
-		root->previous = current_node;
-		root->task->sleeping = 0;
-	}
 	
+	if(amountBlockedTasks > 0) {
+		int amountBlockedTasksRemoved = 0;
+		
+		for(int i = 0; i <  amountBlockedTasks; i++){
+			if(blockedQueue[i]->task->sleepCounter > 0){
+				blockedQueue[i]->task->sleepCounter -= kernel_settings.isrTicks;
+			} else {
+				blockedQueue[i]->task->sleepCounter = 0;
+
+				current_node_ready->next->previous = blockedQueue[i];
+				blockedQueue[i]->next = current_node_ready->next;
+				blockedQueue[i]->previous = current_node_ready;
+				current_node_ready->next = blockedQueue[i];
+				blockedQueue[i] = NULL;
+				amountBlockedTasksRemoved++;
+			}
+		}
+		amountBlockedTasks -= amountBlockedTasksRemoved;
+	}
+
+
 	//Do not switch context on first run
 	if(firstrun){
 		//Only run once
 		firstrun = 0;
 		//Set stack pointer
-		asm volatile("MOV r0, %[lowAddress] " :: [lowAddress] "r" (current_node->task->spl));
+		asm volatile("MOV r0, %[lowAddress] " :: [lowAddress] "r" (current_node_ready->task->spl));
 		asm volatile("OUT __SP_L__, r0");
-		asm volatile("MOV r0, %[highAddress] " :: [highAddress] "r" (current_node->task->sph));
+		asm volatile("MOV r0, %[highAddress] " :: [highAddress] "r" (current_node_ready->task->sph));
 		asm volatile("OUT __SP_H__, r0");
 		
-		asm("MOV R18, %[lowAdress]" :: [lowAdress] "r" (current_node->task->pcl));
+		asm("MOV R18, %[lowAdress]" :: [lowAdress] "r" (current_node_ready->task->pcl));
 		asm("PUSH R18");
-		asm("MOV R18, %[highAdress]" :: [highAdress] "r" (current_node->task->pch));
+		asm("MOV R18, %[highAdress]" :: [highAdress] "r" (current_node_ready->task->pch));
 		asm("PUSH 18");
-		current_node->task->firsttime = 0;
+		current_node_ready->task->firsttime = 0;
 		
 	}
 	if(dispatcher) {
 		//Save context only if task is not marked for removal
-		if(!current_node->task->markedforremoval) {
+		if(!current_node_ready->task->markedforremoval) {
 			//Save context
 			asm volatile (
 			"push  r0                    \n\t" \
@@ -277,7 +306,7 @@ ISR (TIMER1_COMPA_vect)
 			//Store stackpointer in TCB
 			
 			asm volatile("in    r2, __SP_L__");
-			current_node->task->spl = reg1;
+			current_node_ready->task->spl = reg1;
 			//asm("lds r26,%0" :: "Q" (current_node->task->spl));
 			//asm volatile("STS %0,R25", :: "=r" (a));
 			//asm volatile("MOV %[lowAdress], r0 ": [lowAdress] "=r" (a) :);
@@ -285,47 +314,47 @@ ISR (TIMER1_COMPA_vect)
 			//current_node->task->spl = regReader;
 			
 			asm volatile("in    r3, __SP_H__");
-			current_node->task->sph = reg2;
+			current_node_ready->task->sph = reg2;
 			//asm volatile("MOV %[highAdress], r0 ": [highAdress] "=r" (b) : );
 			//current_node->task->sph = b;
 		}
 	
 		//Select the current task
-		if(current_node->next->task->markedforremoval){
+		if(current_node_ready->next->task->markedforremoval){
 			//Remember the task for removal
-			previous_node = current_node->next;
+			temp = current_node_ready->next;
 			//Skip the task for removal
-			current_node->next = current_node->next->next;
+			current_node_ready->next = current_node_ready->next->next;
 			//Set the  previous node to the current node
-			current_node->next->previous = current_node;
+			current_node_ready->next->previous = current_node_ready;
 			//Select the next node
-			current_node = current_node->next;
+			current_node_ready = current_node_ready->next;
 			
 			//Delete the task to be removed
-			free(previous_node->task);
-			previous_node->next = 0;
-			free(previous_node);
+			free(temp->task);
+			temp->next = 0;
+			free(temp);
 		} else {
-			current_node = current_node->next;
+			current_node_ready = current_node_ready->next;
 		}
 		
 		//If the task is running for the first time we do not need to load the context from the stack
-		if(current_node->task->firsttime){
-			asm volatile("MOV r0, %[lowAddress] " :: [lowAddress] "r" (current_node->task->spl));
+		if(current_node_ready->task->firsttime){
+			asm volatile("MOV r0, %[lowAddress] " :: [lowAddress] "r" (current_node_ready->task->spl));
 			asm volatile("OUT __SP_L__, r0");
-			asm volatile("MOV r0, %[highAddress] " :: [highAddress] "r" (current_node->task->sph));
+			asm volatile("MOV r0, %[highAddress] " :: [highAddress] "r" (current_node_ready->task->sph));
 			asm volatile("OUT __SP_H__, r0");
 			
-			asm("MOV R18, %[lowAdress]" :: [lowAdress] "r" (current_node->task->pcl));
+			asm("MOV R18, %[lowAdress]" :: [lowAdress] "r" (current_node_ready->task->pcl));
 			asm("PUSH R18");
-			asm("MOV R18, %[highAdress]" :: [highAdress] "r" (current_node->task->pch));
+			asm("MOV R18, %[highAdress]" :: [highAdress] "r" (current_node_ready->task->pch));
 			asm("PUSH 18");
-			current_node->task->firsttime = 0;
+			current_node_ready->task->firsttime = 0;
 		} else {
 			//Load stack pointers
-			asm volatile("MOV r0, %[lowAddress] " :: [lowAddress] "r" (current_node->task->spl));
+			asm volatile("MOV r0, %[lowAddress] " :: [lowAddress] "r" (current_node_ready->task->spl));
 			asm volatile("OUT __SP_L__, r0");
-			asm volatile("MOV r0, %[highAddress] " :: [highAddress] "r" (current_node->task->sph));
+			asm volatile("MOV r0, %[highAddress] " :: [highAddress] "r" (current_node_ready->task->sph));
 			asm volatile("OUT __SP_H__, r0");
 						
 			//Load context
